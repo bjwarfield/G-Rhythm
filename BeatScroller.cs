82acb4de36eb594e362f8de09387a21687f7ea6d
aglawson <36459896+aglawson@users.noreply.github.com>
2020-02-10 15:51:15 -0800

    Beat Scroller and Note Object Scripts
    Beat Scroller: Script for moving arrows along the screen
    Note Object: When arrow is over a button and the button is pressed, the arrow disappears

---
+++ BeatScroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour {
+
+	public float beatTempo;
+	public bool hasStarted;
+
+	// Use this for initialization
+	void Start () {
+		beatTempo = beatTempo;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(!hasStarted)
+        {
+			if(Input.anyKeyDown)
+            {
+				hasStarted = true;
+            }
+        }
+		else
+        {
+			transform.position -= new Vector3(-beatTempo * Time.deltaTime, 0f, 0f);
+        }
+	}
+}
---
+++ NoteObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour {
+
+	public bool canBePressed;
+	public KeyCode keyToPress;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(keyToPress))
+        {
+			if(canBePressed)
+            {
+				gameObject.SetActive(false);
+            }
+        }
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+    {
+		if(other.tag == "Activator")
+        {
+			canBePressed = true;
+        }
+    }
+	
+	private void OnTriggerExit2D(Collider2D other)
+    {
+		if(other.tag == "Activator")
+        {
+			canBePressed = false;
+        }
+    }
+}
