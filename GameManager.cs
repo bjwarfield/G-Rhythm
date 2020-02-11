02f5dce4b33b4ffef855ee3b5f3e8fa2b72187f4
aglawson <36459896+aglawson@users.noreply.github.com>
2020-02-11 14:26:21 -0800

    GameManager
    *Allows for music input
    *Detects hits and misses

---
+++ GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager: MonoBehaviour {
+
+	public AudioSource theMusic;
+
+	public bool startPlaying;
+
+	public BeatScroller theBS;
+
+	public static GameManager instance;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!startPlaying)
+        {
+			if(Input.anyKeyDown)
+            {
+				startPlaying = true;
+				theBS.hasStarted = true;
+
+				theMusic.Play();
+            }
+        }
+	}
+
+	public void NoteHit()
+    {
+		Debug.Log("Hit on Time");
+    }
+
+	public void NoteMissed()
+    {
+		Debug.Log("Missed");
+    }
+}
