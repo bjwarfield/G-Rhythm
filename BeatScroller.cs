ec4d6b361c4cc49c6ee837180277e6901f1dc283
aglawson <36459896+aglawson@users.noreply.github.com>
2020-02-11 14:30:12 -0800

    Small update
    Commented out lines 20 to 23 because the same action is performed in the GameManager script

--- BeatScroller.cs
+++ BeatScroller.cs
@@ -17,10 +17,10 @@ public class BeatScroller : MonoBehaviour {
 		
 		if(!hasStarted)
         {
-			if(Input.anyKeyDown)
+	    /*		if(Input.anyKeyDown)
             {
 				hasStarted = true;
-            }
+            }*/
         }
 		else
         {
