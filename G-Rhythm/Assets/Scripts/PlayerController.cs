8d382270065c228b9fbbce0ada5cb3c7d4219fcb
BJ Warfield <itsabeejay@yahoo.com>
2020-02-18 00:35:34 -0800

    Added Gameplay Scripts
    Imported code from Adrian's Scripts branch
    Added color coded Circles as notes entities
    Cleaned up folder structure .
    Removed redundant animations.

--- G-Rhythm/Assets/Scripts/PlayerController.cs
+++ G-Rhythm/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool isAttacking;
-    public float speed;
+
+    public KeyCode attackOne;
+    public KeyCode attackTwo;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( Input.GetKeyDown( attackOne ) )
+        {
+            animator.SetTrigger( "Attack" );
+        }
+
+        if (Input.GetKeyDown(attackTwo))
+        {
+            animator.SetTrigger("Slash");
+        }
     }
 }
