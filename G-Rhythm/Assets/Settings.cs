e534cbe7d2308b41d6cb863551bb54045d4998e9
RJ Andaya <rjandaya98@gmail.com>
2020-02-21 22:23:36 -0800

    Cloned from settings2. Implement Music

---
+++ G-Rhythm/Assets/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
+{
+   public AudioMixer audioMixer;
+   public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void btnStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
---
+++ G-Rhythm/Assets/scriptMM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scriptMM : MonoBehaviour
+{
+
+    public void btnStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void btnQuit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void btnBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void btnSettings()
+    {
+        SceneManager.LoadScene("SettingMenu");
+    }
+}
