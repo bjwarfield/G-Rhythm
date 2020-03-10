2926cdf7362977f085a0d1c285d843522a9e79fa
aglawson <36459896+aglawson@users.noreply.github.com>
2020-03-09 20:17:28 -0700

    Updated With BJ's changes

--- G-Rhythm/Assets/Scripts/NoteObject.cs
+++ G-Rhythm/Assets/Scripts/NoteObject.cs
@@ -1,51 +1,64 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoteObject : MonoBehaviour {
+public class NoteObject : MonoBehaviour
+{
 
 	public bool canBePressed;
 	public KeyCode keyToPress;
+	public GameManager GameManager;
 	// Use this for initialization
-	void Start () 
+	void Start()
 	{
 
 	}
 
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(keyToPress))
-        {
-			if(canBePressed)
-            {
-				gameObject.SetActive(false);
+	void Update()
+	{
+		if (Input.GetKeyDown(keyToPress))
+		{
+			if (canBePressed)
+			{
 
-				GameManager.instance.NoteHit();
-            }
-        }
+				gameObject.SetActive(false);
+			
+				float offset = -Mathf.Abs(this.transform.position.x - GameManager.instance.zoneController.transform.position.x);
+				Debug.Log("Click Offset: " +  offset);
+
+				if (offset < 1)
+				{
+					GameManager.instance.ExcellentHit();
+				}
+				else if (offset >= 1 && offset < 2)
+				{
+					GameManager.instance.GoodHit();
+				}
+				else if (offset >= 2)
+				{
+					GameManager.instance.PoorHit();
+				}
+			}
+		}
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
-    {
-		if(other.tag == "Activator")
-        {
-			canBePressed = true;
-        }
-		if (other.tag == "Obstacle")
+	{
+		if (other.tag == "Activator")
 		{
-			gameObject.SetActive(false);
+			canBePressed = true;
 		}
-
 	}
-	
+
 	private void OnTriggerExit2D(Collider2D other)
-    {
+	{
 		if (other.tag == "Activator")
 		{
 			canBePressed = false;
 
-			GameManager.instance.NoteMissed();
+			//GameManager.instance.NoteMissed();
 		}
 	}
-	
+
 }
