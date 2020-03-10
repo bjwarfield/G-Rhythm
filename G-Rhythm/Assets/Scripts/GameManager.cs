336f623e0cc162cd381410b23c3b731b36a2ad1f
aglawson <36459896+aglawson@users.noreply.github.com>
2020-03-09 20:16:25 -0700

    Updated with BJ's changes and my Score Functions

--- G-Rhythm/Assets/Scripts/GameManager.cs
+++ G-Rhythm/Assets/Scripts/GameManager.cs
@@ -1,53 +1,76 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager: MonoBehaviour {
+public class NoteAccuracy
+{
+	public string value;
+
+	private NoteAccuracy(string value) { this.value = value; }
+	public static NoteAccuracy Excellent { get { return new NoteAccuracy("Excellent"); } }
+	public static NoteAccuracy Good { get { return new NoteAccuracy("Good"); } }
+	public static NoteAccuracy Poor { get { return new NoteAccuracy("Poor"); } }
+
+	public static implicit operator string(NoteAccuracy noteAccuracy) { return noteAccuracy.value; }
+}
+public class GameManager : MonoBehaviour
+{
 
 	public AudioSource theMusic;
-
+	public ZoneController zoneController;
 	public bool startPlaying;
 
 	public BeatScroller theBS;
 
 	public static GameManager instance;
-	
-	public float Score = 0;
+
+	private float Score = 0;
+
+	public float PoorScore = 10f;
+	public float GoodScore = 100f;
+	public float ExcellentScore = 1000f;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		instance = this;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		if(!startPlaying)
-        {
-			if(Input.anyKeyDown)
-            {
+	void Update()
+	{
+		if (!startPlaying)
+		{
+			if (Input.anyKeyDown)
+			{
 				startPlaying = true;
 				theBS.hasStarted = true;
 
 				theMusic.Play();
-            }
-        }
-		
-		Debug.Log("Score: " + getScore());
-		
+			}
+		}
 	}
-	
-	public float getScore()
+
+	public void PoorHit()
+	{
+		Score += PoorScore;
+		Debug.Log("Score: " + Score);
+	}
+
+	public void GoodHit()
     {
-	return Score;
+		Score += GoodScore;
+		Debug.Log("Score: " + Score);
     }
 
-	public void NoteHit()
+	public void ExcellentHit()
     {
-		Score += 1;
-    }
+		Score += ExcellentScore;
+		Debug.Log("Score: " + Score);
+	}
 
 	public void NoteMissed()
-    {
+	{
 		Debug.Log("Missed");
-    }
+	}
 }
