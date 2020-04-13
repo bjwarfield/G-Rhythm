841db478f7652e06f802d2686142e737a03d080d
Jonathan Dao <jonathandao99@hotmail.com>
2020-04-13 00:02:00 -0700

    Added end screen menu, currently waiting for true value functions, currently doesn't work. Added Audio sample on song selection

---
+++ G-Rhythm/Assets/Scripts/UI Scripts/scriptEndPop.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class scriptEndPop : MonoBehaviour
+{
+    public static bool GameEnd = false;
+    public GameObject endGamePopUp;
+    public Text txtEndScore;
+    public string name;
+
+    void Start()
+    {
+//        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * if hpBar or end note is true
+         */
+    //    txtEndScore = GetComponent<Text>();
+        //      txtEndScore.text = "Score: " + GameManager.instance.getScore().ToString();
+        showEndPop();
+
+    }
+
+    void showEndPop()
+    {
+        endGamePopUp.SetActive(false);
+        Time.timeScale = 1f;
+        GameEnd = false;
+    }
+
+    void hideEndPop()
+    {
+        endGamePopUp.SetActive(true);
+        Time.timeScale = 0f;
+        GameEnd = true;
+    }
+
+    public void btnSubmit()
+    {
+        SceneManager.LoadScene(2);
+        hideEndPop();
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+}
