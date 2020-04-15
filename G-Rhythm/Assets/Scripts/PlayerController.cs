9bdd5c8c317fd98a928a533064e4dad173a324f0
RJ Andaya <rjandaya98@gmail.com>
2020-04-14 19:13:14 -0700

    Finished Kira - New World Easy & Hard. Started Audioscribe - Shimmer

--- G-Rhythm/Assets/Scripts/PlayerController.cs
+++ G-Rhythm/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,15 @@ public class PlayerController : MonoBehaviour
     public KeyCode attackOne;
     public KeyCode attackTwo;
     private Animator animator;
+    private AudioSource audioSource;
+    public List<AudioClip> swingSounds;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = swingSounds[Random.Range(0, swingSounds.Count)];
     }
 
     // Update is called once per frame
@@ -19,11 +24,15 @@ public class PlayerController : MonoBehaviour
     {
         if( Input.GetKeyDown( attackOne ) )
         {
+            audioSource.clip = swingSounds[Random.Range(0, swingSounds.Count)];
+            audioSource.Play();
             animator.SetTrigger( "Attack" );
         }
 
         if (Input.GetKeyDown(attackTwo))
         {
+            audioSource.clip = swingSounds[Random.Range(0, swingSounds.Count)];
+            audioSource.Play();
             animator.SetTrigger("Slash");
         }
     }
